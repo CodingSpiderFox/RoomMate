36263d9fea8f212ed25468e21176a173acfbe5cb
CodingSpiderFox <sunrisechain@gmail.com>
2018-11-25 21:44:00 +0100

    display working

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,23 @@
+#include <SPI.h>
+#include <Wire.h>
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <version.h>
 
-/*     Arduini PIR Motion Sensor Tutorial
-  *      
-  *  by Dejan Nedelkovski, www.HowToMechatronics.com
-  *  
-  */
 int PIR_SENSOR_PIN = 0;
 int PIXEL_PIN = 4;
 int PIXEL_COUNT = 1;
 int DISPLAY_SDA_PIN = 0;
-int DISPLAY_SCL_PINT = 0;
+int DISPLAY_SCL_PIN = 0;
+#define OLED_RESET_PIN D5
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
+/* Object named display, of the class Adafruit_SSD1306 */
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
+uint8_t s = 0, m = 0, h = 0;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 void colorWipe(uint32_t c, uint8_t wait) {
@@ -30,19 +28,54 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
-void setup() {
+void drawStr(uint8_t x, uint8_t y, char* str){
+  display.setCursor(x, y);  /* Set x,y coordinates */
+  display.println(str);
+}
+
+void writeOLED(){
+  char time[30];
+  display.clearDisplay();  /* Clear display */
+  sprintf(time, "%02d:%02d:%02d", h, m, s);
+  drawStr(50, 10, "Timer");
+  drawStr(40, 30, time);
+  display.println(VERSION);
+  display.display();
+}
+
+void updateWatch() {
+  s = s + 1;
+  if (s == 60) {
+    s = 0;
+    m = m + 1;
+  }
+  if (m == 60) {
+    m = 0;
+    h = h + 1;
+  }
+  if (h == 13)
+    h = 1;
+
+  writeOLED();
+}
+
+
+
+
+void setup()   {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); /* Initialize display with address 0x3C */
   display.clearDisplay();  /* Clear display */
   display.setTextSize(1);	/* Select font size of text. Increases with size of argument. */
   display.setTextColor(WHITE);	/* Color of text*/
-  display.println("1");
   pinMode(PIR_SENSOR_PIN, INPUT);
 
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.show();
 }
 
 void loop() {
+  updateWatch();/* Every second increment clock and display */
+  delay(1000);
   int sensorValue = digitalRead(PIR_SENSOR_PIN);
   if (sensorValue == 1) {
     colorWipe(strip.Color(255, 0, 0), 50);
