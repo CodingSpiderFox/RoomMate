9be93eb36d65450ea60834d73e4029541cb8862e
codingspiderfox <codingspiderfox@gmail.com>
2018-11-25 17:41:01 +0100

    one step closer to get the display working

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 /*     Arduini PIR Motion Sensor Tutorial
   *      
   *  by Dejan Nedelkovski, www.HowToMechatronics.com
   *  
   */
-int pirSensor = 0;
+int PIR_SENSOR_PIN = 0;
 int PIXEL_PIN = 4;
 int PIXEL_COUNT = 1;
+int DISPLAY_SDA_PIN = 0;
+int DISPLAY_SCL_PINT = 0;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
@@ -21,14 +31,19 @@ void colorWipe(uint32_t c, uint8_t wait) {
 }
 
 void setup() {
-  pinMode(pirSensor, INPUT);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); /* Initialize display with address 0x3C */
+  display.clearDisplay();  /* Clear display */
+  display.setTextSize(1);	/* Select font size of text. Increases with size of argument. */
+  display.setTextColor(WHITE);	/* Color of text*/
+  display.println("1");
+  pinMode(PIR_SENSOR_PIN, INPUT);
 
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  int sensorValue = digitalRead(pirSensor);
+  int sensorValue = digitalRead(PIR_SENSOR_PIN);
   if (sensorValue == 1) {
     colorWipe(strip.Color(255, 0, 0), 50);
   }
