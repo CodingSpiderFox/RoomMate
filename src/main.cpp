5099349528aa9e396022532e6ed4a97a462424bf
CodingSpiderFox <sunrisechain@gmail.com>
2018-11-26 22:11:39 +0100

    add some pcf8574 related stuff .. display with dht still working

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,19 @@ const int DHTPin = 15; //D8
 // Initialize DHT sensor.
 DHT dht(DHTPin, DHT_SENSOR_TYPE);
 
+int counter[16];       // how many times we have seen new value
+int reading[16];           // the current value read from the input pin
+int current_state[16];    // the debounced input value
+// the following variable is a long because the time, measured in milliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long time_btn[16];         // the last time the output pin was sampled
+int debounce_count = 10; // number of millis/samples to consider before declaring a debounced input
+int pressCount[16];
+
+SoftwareSerial swSer(D0, D3, false); //TODO is this still needed?
+
 TwoWire wire2;
+TwoWire testWire;
 
 int PIR_SENSOR_PIN = 0;
 int PIXEL_PIN = 4;
@@ -24,13 +36,36 @@ int PIXEL_COUNT = 1;
 int DISPLAY_SDA_PIN = 0; //D2
 int DISPLAY_SCL_PIN = 0; //D1
 int LDR_PIN = 0; //A0
-#define OLED_RESET_PIN D5
+
+#define PIN_PCF8574_INT D5
+#define PIN_PCF8574_SDA D1
+#define PIN_PCF8574_SCL D2
+
+#define OLED_RESET_PIN D5 //just a dummy value. the OLED is not connected to this pin ... TODO: check wheter other devices can still use D5 .. also try to set a pin number here which is not present on the nodemcu module so there will be no interferance
+
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64
 
+#define BRIGHT    300     //max led intensity (1-500)
+#define INHALE    1250    //Inhalation time in milliseconds.
+#define PULSE     INHALE*1000/BRIGHT
+#define REST      1000    //Rest Between Inhalations.
+
+#define ledPin D8
+
+
+int i=0;
+int breathe_delay = 15;   // delay between loops
+unsigned long breathe_time = millis();
+
+
 /* Object named display, of the class Adafruit_SSD1306 */
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
+// Initialize a PCF8574 at I2C-address 0x20, using GPIO5, GPIO4 and testWire for the I2C-bus
+PCF857x pcf8574(0x20, &testWire);
+PCF857x pcf8574_2(0x27, &testWire);
+
 uint8_t s = 0, m = 0, h = 0;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
@@ -89,8 +124,60 @@ void updateWatch() {
   writeOLED();
 }
 
+bool CheckKey(byte key, byte num){ //0, 1, 2, 3
+  return key & (1 << num);
+}
 
+void printSerial(String str) {
+  Serial.print(str);
+  swSer.print(str + '\0');
+  swSer.flush();
+}
 
+void printlnSerial(String str) {
+  printSerial(str + "\r\n");
+}
+
+void handleInterrupt () {
+   if (digitalRead(PIN_PCF8574_INT) == LOW) {
+
+    for (int i = 0; i < sizeof(current_state) / sizeof(current_state[0]); i++) {
+      if (millis() != time_btn[i])
+      {
+        if (i < 8) {
+          reading[i] = pcf8574.read(i);
+        }
+        else {
+          reading[i] = pcf8574_2.read(i-8);
+        }
+    
+        if (reading[i] == current_state[i] && counter[i] > 0)
+        {
+          counter[i]--;
+        }
+        if (reading[i] != current_state[i])
+        {
+           counter[i]++; 
+        }
+        // If the Input has shown the same value for long enough let's switch it
+        if (counter[i] >= debounce_count)
+        {
+          counter[i] = 0;
+          current_state[i] = reading[i];
+          
+          if (pressCount[i]!=1) {
+            ++pressCount[i];            
+            printlnSerial("BTN_" + String(i));
+          }
+          else {
+            --pressCount[i];
+          }
+        }
+        time_btn[i] = millis();
+      }
+    } 
+  }
+}
 
 void setup()   {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); /* Initialize display with address 0x3C */
@@ -99,8 +186,17 @@ void setup()   {
   display.setTextColor(WHITE);	/* Color of text*/
   pinMode(PIR_SENSOR_PIN, INPUT);
 
+  for(int i = 0; i < sizeof(current_state) / sizeof(current_state[0]); i++) {
+    current_state[i] = HIGH;
+    counter[i] = 0;
+    time_btn[i] = 0;
+    reading[i] = 0;
+    pressCount[i] = 0;
+  }
+
   strip.begin();
   strip.show();
+  
   Serial.begin(9600);
 }
 
